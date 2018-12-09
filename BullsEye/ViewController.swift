af1bd41c46c13df3b476b7bfc5a4c0597c79ee9f
Sergio Ballen <silvaralth@gmail.com>
2018-12-09 13:53:22 -0500

    alert

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello world", message: "This is a lorem ipsum", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default , handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func whoisthere() {
+        let alert = UIAlertController(title: "YEEES?", message: "WHO IS THERE?", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Me", style: .destructive, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
