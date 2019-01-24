55e1c99df5a4b4d9a7ac6694bb5bf4657cd0cc0e
Sergio Ballen <silvaralth@gmail.com>
2019-01-24 16:27:00 -0500

    targetLabel

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,27 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue: Int = 50
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
     }
 
     @IBAction func showAlert() {
-        let messageInAlert = "The Slider value is \(currentValue)"
+        let messageInAlert = "Your slider value is \(currentValue)" + "\nThe target was \(targetValue)"
         let alert = UIAlertController(title: "Hello world", message: messageInAlert, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default , handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         let roundedValue = slider.value.rounded()
-        print("The Slider value is \(slider.value) and the rounded is \(roundedValue)")
-        // currentValue = Int(slider.value)
         currentValue = Int(roundedValue)
+        // print("Slider val: \(slider.value) - rounded: \(roundedValue)")
     }
     
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        targetLabel.text = String(targetValue)
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
 }
 
