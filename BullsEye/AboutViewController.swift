40760584cf354aa8e8fabdf979fbffc67063629c
Sergio Ballen <silvaralth@gmail.com>
2019-02-24 23:03:39 -0500

    Author/About Screens

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by ian on 2/23/19.
+//  Copyright © 2019 ian. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ BullsEye/AuthorViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by ian on 2/23/19.
+//  Copyright © 2019 ian. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
